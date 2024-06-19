f0f000210f9847319fee1c3ac0e0bcbc48d8562e
Diego de Biagi <diegobiagiviana@gmail.com>
2024-06-18 23:10:06 -0300

    feat: error handling

--- src/main/kotlin/com/dbiagi/listing/service/ListingService.kt
+++ src/main/kotlin/com/dbiagi/listing/service/ListingService.kt
@@ -3,16 +3,17 @@ package com.dbiagi.listing.service
 import com.dbiagi.listing.client.AccountClient
 import com.dbiagi.listing.config.Exchanges
 import com.dbiagi.listing.config.RoutingKeys
-import com.dbiagi.listing.domain.Account
 import com.dbiagi.listing.domain.CreateListingRequest
-import com.dbiagi.listing.model.Listing
 import com.dbiagi.listing.domain.UpdateListingRequest
+import com.dbiagi.listing.domain.exception.NotFoundException
+import com.dbiagi.listing.model.Listing
 import com.dbiagi.listing.repository.ListingRepository
 import mu.KotlinLogging
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.switchIfEmpty
 import java.time.LocalDateTime
 import java.util.*
 
@@ -29,15 +30,13 @@ class ListingService(
         // check if the owner exists and is valid, if not return an error
         // check if owner has listings available to create, if not return an error
         // after create the listing, update owner listings count
-        return Mono.just(request)
-            .map {
-                logger.info("listing create request request={}", request)
-                toListing(request)
-            }
-            .flatMap { listingRepository.save(it) }
+        logger.info("listing create request request={}", request)
+        val listing = toListing(request)
+
+        return listingRepository.save(listing)
             .map {
                 logger.info("listing created listing={}", it)
-                rabbitMqService.publish(Exchanges.LISTING_CREATED, null, it).then()
+                rabbitMqService.publish(Exchanges.LISTING_CREATED, null, it)
                 it
             }
             .doOnError { ex -> logger.error("error creating listing request=$request", ex) }
@@ -66,33 +65,19 @@ class ListingService(
                     updatedAt = LocalDateTime.now()
                 )
             }
+            .switchIfEmpty { Mono.error(NotFoundException()) }
             .flatMap { listingRepository.save(it) }
             .doOnSuccess { rabbitMqService.publish(Exchanges.LISTING_UPDATED, RoutingKeys.LISTING_UPDATED, it) }
+            .doOnError { ex -> logger.error("error updating listing id=${id}, request=${request}, message=${ex.message}", ex) }
 
     fun getListings(page: Pageable): Flux<Listing> {
         logger.info("get listings page=${page}")
-
         return listingRepository.findAll(page.sort)
     }
 
     fun getFeaturedListings(page: Pageable): Flux<Listing> {
         logger.info("get featured listings page=${page}")
         return listingRepository.findFeatured(page)
-    }
-
-    fun paginated(): Mono<List<Account>> {
-        val firstPage = 1
-        return accountClient.searchPaginated(firstPage)
-            .expand { response ->
-                if(response.hasNextPage())
-                    accountClient.searchPaginated(response.page.next)
-                else
-                    Flux.empty()
-            }
-            .reduce { agg, response ->
-                agg.copy(accounts = agg.accounts + response.accounts)
-            }
-            .map { it.accounts }
     }
 
     fun findByOwnerId(page: Pageable, id: String): Flux<Listing> {
